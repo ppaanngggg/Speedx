aa60a995580e2252774466fb64b7503cf985a068
manfred-exz <manfred.exz@gmail.com>
2015-11-03 15:36:42 +0800

    1. A health slider is added. 2. Screen shake when collided is enabled. Note that a test shake at game start is still kept.

--- Assets/Script/Creater.cs
+++ Assets/Script/Creater.cs
@@ -15,7 +15,7 @@ public class Creater : MonoBehaviour {
 	const int totalBatch = lenBatch + 2 * paddingBatch;
 	const int centerBatch = totalBatch / 2;
 	const int initBatch = 5;
-	const int roundBatch = 12;
+	const int roundBatch = 12;	// number of quads in a circle
 	const int hinderMod = 5;
 	const float angleSpeed = 2.0f;
 	const float disQuad = 1.86f;
@@ -40,19 +40,21 @@ public class Creater : MonoBehaviour {
 		if (lastQuadZ < initBatch * totalBatch)
 			create ();
 
-		if (Input.GetKey (KeyCode.RightArrow))
-			angle -= angleSpeed;
+//		if (Input.GetKey (KeyCode.RightArrow))
+//			angle -= angleSpeed;
+//
+//		if (Input.GetKey (KeyCode.LeftArrow))
+//			angle += angleSpeed;
 
-		if (Input.GetKey (KeyCode.LeftArrow))
-			angle += angleSpeed;
-		
+		// use Input.GetAxis instead, to improve control accuracy.
+		angle -= angleSpeed * Input.GetAxis ("Horizontal");
 	}
 
 	// create a batch of quads
 	void create () {
-		int hinder = Random.Range (0, roundBatch);
+		int hinder = Random.Range (0, roundBatch);	//the offset of a group of hinders	
 		int colorInd = Random.Range (0, colorList.Length);
-		int mod = roundBatch / Random.Range (1, hinderMod + 1);
+		int mod = roundBatch / Random.Range (1, hinderMod + 1);	// one hinder for every [mod] quads in a circle.
 
 		for (int i = 0; i < totalBatch; i++) {
 			for (int j = 0; j < roundBatch; j++) {
@@ -62,7 +64,7 @@ public class Creater : MonoBehaviour {
 				GameObject tmpQuad = (GameObject)Instantiate (Quad);
 				// set special color
 				if (!firstBatch && (j % mod == hinder % mod) && i >= paddingBatch && i < paddingBatch + lenBatch) {
-					float rate = Mathf.Abs(i - centerBatch) / (float)centerBatch;
+					float rate = Mathf.Abs(i - centerBatch) / (float)centerBatch / 10;
 					tmpQuad.GetComponent<Renderer>().material.SetColor(
 						"_Color", colorList[colorInd] * (1.0f - rate) + Color.white * rate
 					);
