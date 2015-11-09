125aa0a6e0b63546731ebea17e85562f49186cfb
John Smith <manfred.exz@gmail.com>
2015-11-09 18:03:04 +0800

    finally succeded in flip a sphere out, a quick blink demo is added.

---
+++ Assets/Script/VisionController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionController : MonoBehaviour {
+	public float blinkSpeed;
+
+	Renderer r;
+
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color color = r.material.color;
+		color.a = (Mathf.Sin (Time.time * blinkSpeed) + 1)/2;
+		r.material.color = color;
+	}
+}
